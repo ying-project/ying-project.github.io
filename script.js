371ff6494ba3d74a160ef3c6efb1f535d2d9c6de
ying-project <zhiyingmeng3108@gmail.com>
2023-06-20 10:47:16 -0400

    fill content for self projects and work

--- script.js
+++ script.js
@@ -45,4 +45,5 @@ function openTab(evt, tabName) {
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
+  document.getElementById("tabid").scrollIntoView(true);
 }
